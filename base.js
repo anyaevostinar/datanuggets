716a17f6eadc523773fd54a17e9a57ba282ce6fb
Anya Johnson <anya@darwin.beacon.msu.edu>
2014-12-19 17:12:38 -0500

    data table not working, saving with ashley's version to try carl's

--- base.js
+++ base.js
@@ -1,4 +1,4 @@
-   var lineData = '{"Data":[{"Avg. Number of Insects":9, "Percent of Damage to Leaves":67.5},{"Avg. Number of Insects":8, "Percent of Damage to Leaves":33.9},{"Avg. Number of Insects":10, "Percent of Damage to Leaves":10.4},{"Avg. Number of Insects":10, "Percent of Damage to Leaves":21.6}]}';
+   var lineData = '{"Data":[{"Loading":9}]}'
    var keys_obj = JSON.parse(lineData);
    var keys = Object.keys(keys_obj.Data[0]);
 
@@ -29,86 +29,41 @@
        self.variableList = ko.observableArray(keys);
 
        //Code for dropdown menus to pick variables to use
-       var1 = new NewVariable("Predictor Variable", self.variableList[0])
-       var2 = new NewVariable("Response Variable", self.variableList[0])
+       var1 = new NewVariable("Predictor Variable", "Select One")
+       var2 = new NewVariable("Response Variable", "Select One")
        self.variables = [var1, var2];
 
        self.data = ko.observableArray(data);
-       
        // grid view model binds to this array of columns
-       self.currentColumns = ko.observableArray([var1.column, var2.column]);
-                
-   //    self.gridViewModel = new ko.simpleGrid.viewModel({
-    //    data: self.data,
-      //     columns: self.currentColumns,
-     //   pageSize: 10
-    //});
+       self.currentColumns = ko.observableArray([{headerText:var1.col_name(),rowText:function(item) {return item[var1.col_name()]}},{headerText:var2.col_name(),rowText:function(item) {return item[var2.col_name()]}}])
+
+       self.gridViewModel = new ko.simpleGrid.viewModel({
+        data: self.data,
+           columns: self.currentColumns,
+        pageSize: 4
+    });
    };
 
    var myData = keys_obj.Data
 
-   var myViewModel = new DNViewModel(myData);
+   var myViewModel = new DNViewModel(keys_obj.Data);
    ko.applyBindings(myViewModel);
 
   $.getJSON("http://beacon-center.org/datanuggets/nuggets/datasets.json", function(data) {
                myViewModel.optionValues(data)
                });
 
-
-
-
-  //TODO: Generate and update axis labels automatically with variable names by hard pulling
-
-  x_value = myViewModel.variables[0].col_name();
+  var x_value;
   x_data = ['x'];
 
-  for (i=0; i<myViewModel.data().length; i++){
-					 x_data.push(myViewModel.data()[i][x_value]);
-					 }
-
-  y_value = myViewModel.variables[1].col_name();
   y_data = ['y'];
+  var y_value
 
-  for (i=0; i<myViewModel.data().length; i++){
-    y_data.push(myViewModel.data()[i][y_value]);
-    }
-
-//~~~~~~~~~~~Graph~~~~~~~~~~~~
-  var chart = c3.generate({
-                          bindto: '#chart',
-                          data: {
-                            x: 'x',
-                            columns: [
-                                      x_data,
-                                      y_data
-                            ],
-                            type: 'scatter'
-                          },
-                          legend: {
-                            show: false
-                          },
-
-                          axis: {
-                            x: {
-                              label: {
-                                text: x_value,
-                                position: 'outer-center'
-                              }
-                            },
-                            y: {
-                              label: {
-                              text: y_value,
-                              position: 'outer-middle'
-                              }
-                            }
-                          }
-
-  });
-
+      
 //~~~~~~~~~~~Update functions~~~~~~~~~~~~
 
  //Update column name choices when data set is chosen
- /*myViewModel.datasets[0].col_name.subscribe(
+ myViewModel.datasets[0].col_name.subscribe(
                                                function(newValue) {
                                                var tempstring = "http://beacon-center.org/datanuggets/nuggets/"+newValue+"/columns.json";
                                                $.getJSON(tempstring, function(data) {
@@ -117,15 +72,16 @@
                                                })
                                                });
 
-*/
+
 
   myViewModel.variables[0].col_name.subscribe(
       function(newValue) {
-        //TODO: check if data is already in dictionary
+	  if(newValue != "Select One"){
+      //TODO: check if data is already in dictionary
         //Else add it to the data items
-        /*var tempstring = "http://beacon-center.org/datanuggets/nuggets/"+myViewModel.datasets[0].col_name()+"/"+newValue+".json";
+        var tempstring = "http://beacon-center.org/datanuggets/nuggets/"+myViewModel.datasets[0].col_name()+"/"+newValue+".json";
         $.getJSON(tempstring, function(data){
-           var x_value = newValue;
+           x_value = newValue;
            x_data = ['x'];
   
         for (i=0; i<data.length; i++){
@@ -136,16 +92,9 @@
               myViewModel.data().push({newValue:parseInt(data[i])})
             }
         }myViewModel.data.valueHasMutated()
-        })*/
+	     myViewModel.currentColumns = [{headerText:newValue,rowText:function(item) {return item[newValue]}},{headerText:myViewModel.variables[1].col_name(),rowText:function(item) {return item[myViewModel.variables[1].col_name()]}}]
         
-        x_value = newValue;
-        x_data=['x']
-        for (i=0; i<myViewModel.data().length; i++){
-					 x_data.push(myViewModel.data()[i][x_value]);
-					 }
-
-                                 
-
+	if (y_value){
                                   var chart = c3.generate({
                                   bindto: '#chart',
                                     data: {x: 'x',
@@ -161,16 +110,18 @@
                                       text: y_value,
                                       position: 'outer-middle'}
                                     }
-                                  }});
+                                  }})}
 
 
-       });
+	    })
+	    }});
 
   myViewModel.variables[1].col_name.subscribe(
                  function(newValue) {
-			//TODO: check if data is already in dictionary
+		     if (newValue != "Select One"){
+		     //TODO: check if data is already in dictionary
 			//Else add it to the data items
-			/*var tempstring = "http://beacon-center.org/datanuggets/nuggets/"+myViewModel.datasets[0].col_name()+"/"+newValue+".json";
+			var tempstring = "http://beacon-center.org/datanuggets/nuggets/"+myViewModel.datasets[0].col_name()+"/"+newValue+".json";
 			$.getJSON(tempstring, function(data){
 			  y_value = newValue;
 			  y_data = ['y'];
@@ -182,15 +133,9 @@
                                   } else {
                                     myViewModel.data().push({newValue:parseInt(data[i])})
                                   }
-                                  }myViewModel.data.valueHasMutated()
-                                  })
-                                  */
-        y_value = newValue;
-        y_data =['y'];
-        for (i=0; i<myViewModel.data().length; i++){
-					 y_data.push(myViewModel.data()[i][x_value]);
-					 }
-
+			  }myViewModel.data.valueHasMutated()
+			       myViewModel.currentColumns = [{headerText:myViewModel.variables[0].col_name(),rowText:function(item) {return item[myViewModel.variables[0].col_name()]}},{headerText:newValue,rowText:function(item) {return item[newValue]}}]
+				       if(x_value){
                           var chart = c3.generate({bindto: '#chart',
                                     data: {x: 'x',
                                     columns: [x_data,y_data],
@@ -205,8 +150,9 @@
                                       text: y_value,
                                       position: 'outer-middle'}
                                     }
-                                  }});
-                  });
+                                  }})}
+			    })
+			    }});
 
 
 
